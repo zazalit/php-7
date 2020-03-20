aff3ce9b1d077f72775ae98197b02d31863143bb
zazalit <61734160+zazalit@users.noreply.github.com>
2020-03-20 06:16:06 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Zaza" => [
+        "city" => "Lyon",
+        "weapon" => "Colt revolver"
+    ]
+];
+
+var_dump($characters);
